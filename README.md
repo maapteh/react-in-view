f483c29e7b354877fcd0ab60821832142447c5dd
Mаартен <Maarten.Oudenniel.van@ahold.nl>
2019-12-12 13:31:16 +0100

    feature(component): render fallback on server allready this makes it nicer on client-side

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # @mpth/react-in-view
+
 > React component to wrap a non SSR component and only show it when it comes into our viewport, speeding up the eventloop while rendering pages SSR and only truly load/show it when it comes into the users view. Works with React >= 16.5.0
 
 When working with Server Side Rendering(SSR) enabled apps, you have to deal with client only components. This wrapper makes it simple to work with those components when you wanted to load data and or when the component comes into the view. Make sure you have 'react-intersection-observer' installed in your application.
@@ -12,7 +13,8 @@ yarn add @mpth/react-in-view
 ```
 
 ### Online demonstration
-*Heroku container spins down when no activity, please be patient!*. See [graphql-schiphol.herokuapp.com](https://graphql-schiphol.herokuapp.com/lazy) for a live demonstration. I will add some storybook/codepen examples overhere too but this lazy example shows how it can be used.
+
+_Heroku container spins down when no activity, please be patient!_. See [graphql-schiphol.herokuapp.com](https://graphql-schiphol.herokuapp.com/lazy) for a live demonstration. I will add some storybook/codepen examples overhere too but this lazy example shows how it can be used.
 
 On [line 13](https://github.com/maapteh/graphql-modules-app/blob/master/packages/app/src/modules/App.tsx#L13) we added the intersection-observer polyfill for IE11. The code for the page itself comes from [lazy.js](https://github.com/maapteh/graphql-modules-app/blob/master/packages/app/src/pages/lazy.js). Now if you watch this page when its spinned up you can see the last two calls are being only made when the user scrolls to them.
 
@@ -27,13 +29,13 @@ import Foo from '../modules/foo';
 import Bar from '../modules/bar';
 
 const Page = () => (
-  <>
-    <h1>Page</h1>
-    <Bar />
-    <InView>
-      <Foo />
-    </InView>
-  </>
+    <>
+        <h1>Page</h1>
+        <Bar />
+        <InView>
+            <Foo />
+        </InView>
+    </>
 );
 ```
 
@@ -47,27 +49,28 @@ options:
 
 ### ad 1. Render a Component on SSR with a personal fallback
 
-Usually, we need to add some loading text or similar until `<Foo />` component starts to render. Here's how to do it.
+Usually, we need to add some loading text or similar until `<Foo />` component starts to render. This fallback is rendered server-side. Here's how to do it.
 
 ```js
-const Loading = () => (<div>Loading...</div>);
+const Loading = () => <div>Loading...</div>;
 const Page = () => (
-  <>
-    ....
-    <InView fallback={<Loading />}>
-      <Foo />
-    </InView2>
-  </>
+    <>
+        ....
+        <InView fallback={<Loading />}>
+            <Foo />
+        </InView2>
+    </>
 );
 ```
 
 Now `<Loading />` component will be rendered until `<Foo />` component comes in the viewport.
 
 ### ad 2.
+
 Set of values serves to grow or shrink each side of the root element's bounding box before computing intersections. For example '20px 0px 280px 0px'
 
-### ad 3. 
+### ad 3.
+
 Add your own css to the main container for example to set a min-height, or aspect ratio or preloading.
 
 [![Codecov Coverage](https://img.shields.io/codecov/c/github/maapteh/react-in-view/master.svg?style=flat-square)](https://codecov.io/gh/maapteh/react-in-view/)
-
--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,11 +1,10 @@
 import React, { CSSProperties } from 'react';
-import ReactDOMServer from "react-dom/server";
+import ReactDOMServer from 'react-dom/server';
 import { render, cleanup } from '@testing-library/react';
-import { mockAllIsIntersecting, mockIsIntersecting, intersectionMockInstance } from 'react-intersection-observer/test-utils'
+import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils';
 import NoSSRinView from './';
 
 describe('NoSSRinView Component', () => {
-
     const style: CSSProperties = {
         height: '101vh',
         textAlign: 'center',
@@ -15,11 +14,10 @@ describe('NoSSRinView Component', () => {
         justifyContent: 'center',
         backgroundColor: '#2d1176',
         color: '#fff',
-    }
+    };
 
     // TODO add mocking window
     describe('on Client Side', () => {
-
         afterEach(() => {
             cleanup();
         });
@@ -27,38 +25,59 @@ describe('NoSSRinView Component', () => {
         const MyComp = () => <div>Hello</div>;
 
         it('should render correctly its inner component when it comes into our view', () => {
-
-            const { queryByText, debug } = render(
+            const { queryByText } = render(
                 <>
                     <section style={{ ...style }}>above</section>
                     <NoSSRinView>
                         <MyComp />
                     </NoSSRinView>
-                </>
+                </>,
             );
             expect(queryByText(/Hello/)).toBeNull();
 
-            mockAllIsIntersecting(true)
+            mockAllIsIntersecting(true);
 
             expect(queryByText(/Hello/)).toBeDefined();
         });
 
-
         it('should be able to set its own bounding box', () => {
-
-            const { queryByText, debug } = render(
+            const { queryByText } = render(
                 <>
                     <section style={{ ...style }}>above</section>
                     <NoSSRinView rootMargin="40px 0px 400px 0px">
                         <MyComp />
                     </NoSSRinView>
                     <section style={{ ...style }}>below</section>
-                </>
+                </>,
             );
             expect(queryByText(/Hello/)).toBeNull();
 
-            mockAllIsIntersecting(true)
+            mockAllIsIntersecting(true);
 
+            expect(queryByText(/Hello/)).toBeDefined();
+        });
+
+        it('should have placeholder first and then component', () => {
+            const Loading = () => <div>Loading...</div>;
+            const { queryByText, getByText } = render(
+                <>
+                    <section style={{ ...style }}>above</section>
+                    <NoSSRinView
+                        rootMargin="40px 0px 400px 0px"
+                        fallback={<Loading />}
+                    >
+                        <MyComp />
+                    </NoSSRinView>
+                    <section style={{ ...style }}>below</section>
+                </>,
+            );
+
+            getByText(/Loading/);
+            expect(queryByText(/Hello/)).toBeNull();
+
+            mockAllIsIntersecting(true);
+
+            expect(queryByText(/Loading/)).toBeNull();
             expect(queryByText(/Hello/)).toBeDefined();
         });
 
@@ -66,23 +85,25 @@ describe('NoSSRinView Component', () => {
     });
 
     describe('on Server Side', () => {
-
         const MyComp = () => <div>Hello</div>;
         const Loading = () => <div>Loading...</div>;
 
         it('should render correctly with custom loading', () => {
-            const markup = ReactDOMServer.renderToStaticMarkup(<NoSSRinView fallback={<Loading />}>
-                <MyComp />
-            </NoSSRinView>);
-            expect(markup).toBe('<div><div>Loading...</div></div>')
+            const markup = ReactDOMServer.renderToStaticMarkup(
+                <NoSSRinView fallback={<Loading />}>
+                    <MyComp />
+                </NoSSRinView>,
+            );
+            expect(markup).toBe('<div><div>Loading...</div></div>');
         });
 
         it('should render correctly with no custom loading', () => {
-            const markup = ReactDOMServer.renderToStaticMarkup(<NoSSRinView>
-                <MyComp />
-            </NoSSRinView>);
-            expect(markup).toBe('<div></div>')
+            const markup = ReactDOMServer.renderToStaticMarkup(
+                <NoSSRinView>
+                    <MyComp />
+                </NoSSRinView>,
+            );
+            expect(markup).toBe('<div></div>');
         });
     });
-
 });
